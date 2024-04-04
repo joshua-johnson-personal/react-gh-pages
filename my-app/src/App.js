c43924d7ce53bf64bba9d647e8786df8096a1a83
Joshua Johnson <164550893+joshua-johnson-personal@users.noreply.github.com>
2024-04-04 17:49:22 +0000

    stable

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -24,10 +24,6 @@ function App() {
           <div className="Build-card">
             <h3>Warden Healer</h3>
             <img src="https://raw.githubusercontent.com/two-jay-git/react-gh-pages/main/my-app/src/Warden_Healer.png"
-<<<<<<< HEAD
-=======
-            alt="description of image"></img>
->>>>>>> refs/remotes/origin/main
             style={{ width: '200px', height: 'auto' }} // Custom resize
             />
             <p>A Tanky Oakensoul Warden Healer with Amazing HOTS and Incredible Burst Healing. Stay by their side, if you don't want to die!</p>
@@ -37,10 +33,6 @@ function App() {
           <div className="Build-card">
             <h3>Templar Healer</h3>
             <img src="https://raw.githubusercontent.com/two-jay-git/react-gh-pages/main/my-app/src/Templar_Healer.png"
-<<<<<<< HEAD
-=======
-            alt="description of image"></img>
->>>>>>> refs/remotes/origin/main
             style={{ width: '200px', height: 'auto' }} // Custom resize
             />
             <p>An Ultimate Generating Oakensoul Heal Bot with almost no downtime on their Ultimate Ability. Not as Tanky, but the heals are incredible!</p>
@@ -50,10 +42,6 @@ function App() {
           <div className="Build-card">
             <h3>Nightblade Healer</h3>
             <img src="https://raw.githubusercontent.com/two-jay-git/react-gh-pages/main/my-app/src/Nightblade_Healer.png"
-<<<<<<< HEAD
-=======
-            alt="description of image"></img>
->>>>>>> refs/remotes/origin/main
             style={{ width: '200px', height: 'auto' }} // Custom resize
             />
             <p>A Super Tanky, but sneaky, Nightblade Healer with extreme Survivability and enough damage to steal kills from time to time. Don't let this healer catch you or you might die!</p>
@@ -63,10 +51,6 @@ function App() {
           <div className="Build-card">
             <h3>Arcanist Healer</h3>
             <img src="https://raw.githubusercontent.com/two-jay-git/react-gh-pages/main/my-app/src/Arcanist_Healer.png"
-<<<<<<< HEAD
-=======
-            alt="description of image"></img>
->>>>>>> refs/remotes/origin/main
             style={{ width: '200px', height: 'auto' }} // Custom resize
             />
             <p>An Incredible Arcanist Healer that will heal and buff the group constantly. Giving out the most buffs in the game, your group will be stacked and never die!</p>
@@ -76,10 +60,6 @@ function App() {
           <div className="Build-card">
             <h3>Dragonknight Healer</h3>
             <img src="https://raw.githubusercontent.com/two-jay-git/react-gh-pages/main/my-app/src/Dragonknight_Healer.png"
-<<<<<<< HEAD
-=======
-            alt="description of image"></img>
->>>>>>> refs/remotes/origin/main
             style={{ width: '200px', height: 'auto' }} // Custom resize
             />
             <p>A Super Survivable Dragonknight Healer that will keep you alive and deal damage at the same time. You'll never run out of resources on this build.</p>
@@ -89,10 +69,6 @@ function App() {
           <div className="Build-card">
             <h3>Necromancer Healer</h3>
             <img src="https://raw.githubusercontent.com/two-jay-git/react-gh-pages/main/my-app/src/Necro_Healer.png"
-<<<<<<< HEAD
-=======
-            alt="description of image"></img>
->>>>>>> refs/remotes/origin/main
             style={{ width: '200px', height: 'auto' }} // Custom resize
             />
             <p>Survivability is what this build brings. Death, Siphoning Souls and Consuming Corpses helps out as well.</p>
@@ -102,10 +78,6 @@ function App() {
           <div className="Build-card">
             <h3>Sorcerer Healer</h3>
             <img src="https://raw.githubusercontent.com/two-jay-git/react-gh-pages/main/my-app/src/Sorcerer_Healer.png"
-<<<<<<< HEAD
-=======
-            alt="description of image"></img>
->>>>>>> refs/remotes/origin/main
             style={{ width: '200px', height: 'auto' }} // Custom resize
             />
             <p>An Incredibly Survivable Sorcerer Healer that reduces enemies shields and keeps the group alive. Throw in one of the most powerful Ulitmates in the game, Negate, and you have a real chance of wiping large groups.</p>
@@ -115,10 +87,6 @@ function App() {
           <div className="Build-card">
             <h3>Werewolf Healer</h3>
             <img src="https://raw.githubusercontent.com/two-jay-git/react-gh-pages/main/my-app/src/Werewolf_Healer.png"
-<<<<<<< HEAD
-=======
-            alt="description of image"></img>
->>>>>>> refs/remotes/origin/main
             style={{ width: '200px', height: 'auto' }} // Custom resize
             />
             <p>You are not insane. You read that right. A Werewolf Healer that heals in both human form and werewolf form. If you don't believe me, come and find out!</p>
