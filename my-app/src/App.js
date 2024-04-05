b3493e090028415442de136526b3642e4e22ba3a
Joshua Johnson <164550893+joshua-johnson-personal@users.noreply.github.com>
2024-04-05 23:07:24 +0000

    commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,7 +3,13 @@ import { Routes, Route} from 'react-router-dom';
 import HomePage from './HomePage';
 import AboutPage from './AboutPage';
 import ContactPage from './ContactPage';
-
+const router = createBrowserRouter([{ path: "/", element: <div>Test page</div> }]);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(  
+  <React.StrictMode>    
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
 function App() {
   return (
     <div>
