1d587aa5241514cc5d85bc13ef2ba00a1809470f
Joshua Johnson <164550893+joshua-johnson-personal@users.noreply.github.com>
2024-04-05 23:24:50 +0000

    commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,18 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './HomePage';
 import AboutPage from './AboutPage';
 import ContactPage from './ContactPage';
-import { BrowserRouter as Routes, Route} from 'react-router-dom';
+
 function App() {
   return (
-    <div>
-        <Routes>
-          <Route path="/" element={ <HomePage />}></Route>
-          <Route path="/about" element={ <AboutPage />}></Route>
-          <Route path="/contact" element={ <ContactPage />}></Route>
-        </Routes>
-    </div>
-
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Routes>
+    </Router>
   );
 }
 
