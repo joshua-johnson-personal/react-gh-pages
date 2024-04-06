ef8935d052363bc4836118cf9f4947dea687489d
Joshua Johnson <164550893+joshua-johnson-personal@users.noreply.github.com>
2024-04-06 00:48:39 +0000

    commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import { HashRouter as Routes, Route } from 'react-router-dom';
 import HomePage from './HomePage';
 import AboutPage from './AboutPage';
 import ContactPage from './ContactPage';
+import { Routes, Route, HashRouter } from "react-router-dom";
+
 
 function App() {
   return (
 
+<HashRouter>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<HomePage />}></Route>
-        <Route path="/about" element={<AboutPage />}></Route>
-        <Route path="/contact" element={<ContactPage />}></Route>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+
       </Routes>
+      <Footer />
+</HashRouter>
 
   );
 }
