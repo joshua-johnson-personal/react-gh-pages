aaa3192eb75c2babdf60e9e46cdaa5e46ed27a03
Joshua Johnson <164550893+joshua-johnson-personal@users.noreply.github.com>
2024-04-06 00:39:31 +0000

    commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Routes, Route } from 'react-router-dom';
 import HomePage from './HomePage';
 import AboutPage from './AboutPage';
 import ContactPage from './ContactPage';
 
 function App() {
   return (
-    <Router>
+
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/contact" element={<ContactPage />} />
       </Routes>
-    </Router>
+
   );
 }
 
